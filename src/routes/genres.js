599eb78b671fc51b28caf0fa3e3b719add6190f6
Eugenia Salazar Acuña <90300459+eusalazar@users.noreply.github.com>
2022-07-15 13:26:44 -0400

    add blank space@

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -2,6 +2,7 @@ const { Genres } = require ('../db.js');
 
 const handler = async (req, res) => {
     try {
+
         const allGenres = await Genres.findAll();
         return res.status(200).json(allGenres);
         
